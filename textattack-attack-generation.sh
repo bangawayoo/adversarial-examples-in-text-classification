af04eec71ccf96d754a4af8d45ad32fa9d9e841b
monkilee <myungkilee89@gmail.com>
2021-08-04 20:59:50 +0900

    edit on shell

--- textattack-attack-generation.sh
+++ textattack-attack-generation.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 DATASET="imdb"
-RECIPE="clare"
-MODEL="roberta-base-${DATASET} bert-base-uncased-${DATASET}"
+RECIPE="tf-adj"
+MODEL="bert-base-uncased-${DATASET} roberta-base-${DATASET}"
 for recipe in $RECIPE
 do
   for model in $MODEL
   do
     LOG_FILE_NAME="${model}_${recipe}.txt"
-    textattack attack --recipe $recipe --model $model --num-examples 2000 --log-to-csv ./attack-log/ --model-batch-size 64 --parallel 2>&1 | tee attack-log/$LOG_FILE_NAME
+    textattack attack --attack-from-file recipes/textfooler_jin_2019_adjusted.py --model $model --num-examples 2000 --log-to-csv ./attack_log/ --model-batch-size 64 --parallel 2>&1 | tee attack_log/$LOG_FILE_NAME
   done
 done
 
