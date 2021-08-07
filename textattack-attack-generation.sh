a948944797050a7395592c614459e16b982c7238
KiYoon <bangawayoo@gmail.com>
2021-08-07 15:58:00 +0900

    edit shell

--- textattack-attack-generation.sh
+++ textattack-attack-generation.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
 DATASET="imdb"
-RECIPE="tf-adj"
+RECIPE="pwws textfooler bae"
 MODEL="bert-base-uncased-${DATASET} roberta-base-${DATASET}"
 for recipe in $RECIPE
 do
   for model in $MODEL
   do
-    LOG_FILE_NAME="${model}_${recipe}.txt"
-    textattack attack --attack-from-file recipes/textfooler_jin_2019_adjusted.py --model $model --num-examples 25000 --log-to-csv ./attack-log/$LOG_FILE_NAME.csv --model-batch-size 64 --parallel 2>&1 | tee attack-log/$LOG_FILE_NAME
+    LOG_FILE_NAME="${model}_${recipe}"
+    textattack attack --model $model --num-examples 10000 --log-to-csv "attack-log/$LOG_FILE_NAME.csv" --model-batch-size 64 --parallel 2>&1 | tee "attack-log/$LOG_FILE_NAME.txt"
   done
 done
 
+
+
+#DATASET="imdb"
+#RECIPE="tf-adj"
+#MODEL="bert-base-uncased-${DATASET} roberta-base-${DATASET}"
+#for recipe in $RECIPE
+#do
+#  for model in $MODEL
+#  do
+#    LOG_FILE_NAME="${model}_${recipe}"
+#    textattack attack --attack-from-file recipes/textfooler_jin_2019_adjusted.py --model $model --num-examples 10000 --log-to-csv "attack-log/$LOG_FILE_NAME.csv" --model-batch-size 64 --parallel 2>&1 | tee "attack-log/$LOG_FILE_NAME.txt"
+#  done
+#done
