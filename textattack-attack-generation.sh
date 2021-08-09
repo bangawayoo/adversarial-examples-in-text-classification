a8246ed219c70aaea56bbea3e15c38c292300145
KiYoon <bangawayoo@gmail.com>
2021-08-09 15:11:38 +0900

    edit on shell

--- textattack-attack-generation.sh
+++ textattack-attack-generation.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0
 DATASET="imdb"
-RECIPE="pwws textfooler"
+#RECIPE="pwws textfooler bae"
+RECIPE="pwws"
 MODEL="roberta-base-${DATASET}"
 for recipe in $RECIPE
 do
   for model in $MODEL
   do
     LOG_FILE_NAME="${model}_${recipe}"
-    textattack attack --model $model --num-examples 10000 --log-to-csv "attack-log/$LOG_FILE_NAME.csv" --model-batch-size 64 --recipe $recipe 2>&1 | tee "attack-log/$LOG_FILE_NAME.txt"
+    textattack attack --model $model --num-examples 10000 --log-to-csv "attack-log/$LOG_FILE_NAME.csv" --model-batch-size 64 --recipe $recipe \
+     --num-workers-per-device 4 --checkpoint-interval 500 --checkpoint-dir "attack-log/checkpoint" \
+     2>&1 | tee "attack-log/$LOG_FILE_NAME.txt"
   done
 done
 
