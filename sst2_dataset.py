e746957de36a023137101803bc698c9852288c08
bangawayoo <bangawayoo@gmail.com>
2021-11-06 15:42:42 +0900

    clean unused files

--- sst2_dataset.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from textattack.datasets import Dataset
-
-path = "data/test.csv"
-dataset = pd.read_csv(path, header=None, sep='\t')
-dataset = list(dataset.itertuples(index=False, name=None))
-dataset = Dataset(dataset)
